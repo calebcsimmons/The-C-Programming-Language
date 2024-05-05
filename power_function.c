7dcf4a4ce6d02bef6b15a5be5791278938a9ccb3
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-05-05 08:17:24 -0700

    Added exponential function

---
+++ power_function.c
@@ -0,0 +1,27 @@
+// Function to raise n number to the x power (n^x)
+
+#include <stdio.h>
+
+// First you must declare a function. 
+// Common practice is to declare and define it in the beginning.
+
+// Function Declaration and Definition
+int power (int n, int x) {
+    int result = 1;
+
+    for (int i = 0; i < x; ++i) {
+        result = result * n;
+        }
+    return result;
+
+}
+
+int main () {
+    int num1 = 2;
+    int num2 = 3;
+
+    // Call the function
+    int result = power(num1, num2);
+    printf("%d to the power of %d = %d\n", num1, num2, result);
+
+}
