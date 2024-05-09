e597cb2ac6a7c26fa17f36d6072f6cb78ce154cb
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-05-09 16:20:45 -0700

    chosing headers

--- data_type_ranges.c
+++ data_type_ranges.c
@@ -1,3 +1,6 @@
 // A program that determines the ranges of char, short, int, and long variables, 
 // both signed and unsigned, by printing the appropriate values from standard headers and by direct computation. 
 
+#include <limits.h>
+#include <float.h>
+
