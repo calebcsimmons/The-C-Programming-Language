f435bd761a75c91e4ac14f44b74f3f9a0b290906
Caleb C Simmons <103397543+calebcsimmons@users.noreply.github.com>
2024-05-08 21:10:58 -0700

    hypotenuse_calculator

--- disregard.c
+++ disregard.c
@@ -1,1 +1,20 @@
-type arrayName[arraySize];
+// External Variables and Scope
+
+#include <stdio.h>
+#include <math.h>
+
+#define SIDE1 5
+
+double calculate_hypotenuse(double a, double b) {
+    double hypotenuse = sqrt(a*a + b*b);
+    return hypotenuse;
+}
+
+int main () {
+    int Side2 = 4;
+    
+    double result = calculate_hypotenuse(SIDE1, Side2);
+    printf("%f",result);
+
+    
+}
